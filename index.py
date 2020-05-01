8815850121c5df9097c1a5ea78e57888452ce389
tiavina-mika <ralainirinatiavina@gmail.com>
2020-05-01 09:19:58 +0300

    update

--- index.py
+++ index.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "<h1>Hi there !! Welcome to our first app using Python and Flask</h1>"
+    return "<h1>Hi there !! Welcome to our first app using Python and Flask by Mika</h1>"
