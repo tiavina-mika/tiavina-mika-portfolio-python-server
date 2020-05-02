779a1e8a55ba24ca13bb0b37ed36d10f8bd82a3f
tiavina-mika <ralainirinatiavina@gmail.com>
2020-05-02 12:09:14 +0300

    add cors

--- config/app.py
+++ config/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
 app.config['MONGODB_SETTINGS'] = {
     # 'host': 'mongodb://localhost/tiavina-mika-portfolio'
     'host': 'mongodb+srv://tiavinamika:makemehigh@cluster0-j2yxr.mongodb.net/test?retryWrites=true&w=majority'
